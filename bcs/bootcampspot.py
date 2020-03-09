5f9c77443e40d2e6ca182bf8ec2a8991ac329897
Wired361 <justin.parker.12@gmail.com>
2020-03-09 01:13:46 -0500

    clean up func names and session masks

--- bcs/bootcampspot.py
+++ bcs/bootcampspot.py
@@ -11,20 +11,20 @@ class Bootcampspot:
     def __init__(self, email: str, password: str):
         '''Get Auth and call `/me` endpoint for course info.'''
 
-        _creds = {"email": email,
-                  "password": password}
+        self.__creds = {"email": email,
+                        "password": password}
 
-        self._bcs_root = "https://bootcampspot.com/api/instructor/v1"
-        response = requests.post(
-            f"{self._bcs_root}/login", json=_creds)
-        self._auth = response.json()['authenticationInfo']['authToken']
-        self._head = {
+        self.__bcs_root = "https://bootcampspot.com/api/instructor/v1"
+        __login = requests.post(
+            f"{self.__bcs_root}/login", json=self.__creds)
+        self.__auth = __login.json()['authenticationInfo']['authToken']
+        self.__head = {
             'Content-Type': 'application/json',
-            'authToken': self._auth
+            'authToken': self.__auth
         }
         # Get relevent values
         me = requests.get(
-            self._bcs_root + "/me", headers=self._head).json()
+            self.__bcs_root + "/me", headers=self.__head).json()
         self.user = me['userAccount']
 
         self.class_details = [{'courseName': enrollment['course']['name'], 'courseId': enrollment['courseId'], 'enrollmentId': enrollment['id']}
@@ -38,37 +38,35 @@ class Bootcampspot:
         self.my_cohorts = [course['courseName']
                            for course in self.class_details]
 
-        self._course = None
-        self._enrollment = None
+        self.__course = None
+        self.__enrollment = None
 
     def __repr__(self):
         '''Return course details on print'''
 
-        # Look, I was up waaaay too late and the idea popped into my head so I did it.
-        # I realize the absurdity of what I've done.  But I won't say I'm not proud of it.
-        _rows = len(self.my_courses)
-        _max_name = max([len(str(x)) for x in self.my_cohorts])
+        rows = len(self.my_courses)
+        max_name = max([len(str(x)) for x in self.my_cohorts])
 
         if os.isatty(0):
-            _console_width, _console_height = os.get_terminal_size()
-            if _rows+1 <= _console_height and _console_width >= _max_name+40:
-                _name_fill = ' '*(_max_name-11)
+            console_width, console_height = os.get_terminal_size()
+            if rows+1 <= console_height and console_width >= max_name+40:
+                name_fill = ' '*(max_name-11)
 
                 def _repr_row_gen() -> Generator:
                     for idx, value in enumerate(reversed(self.class_details)):
                         course_pad = ' '*(9-len(str(value['courseId'])))
                         enroll_pad = ' '*(13-len(str(value['enrollmentId'])))
                         name_pad = ' ' * \
-                            (_max_name-len(str(value['courseName'])))
+                            (max_name-len(str(value['courseName'])))
                         output_string = f"{idx}| {name_pad}{value['courseName']} |{course_pad}{value['courseId']} |{enroll_pad}{value['enrollmentId']} |\n"
                         yield output_string
-                _header = f" |{_name_fill}  Class Name | courseID | enrollmentId |\n"
-                _output = StringIO("")
-                _output.write(_header)
-                _output.write(f" {'-'*(len(_header)-2)}\n")
+                header = f" |{name_fill}  Class Name | courseID | enrollmentId |\n"
+                output = StringIO("")
+                output.write(header)
+                output.write(f" {'-'*(len(header)-2)}\n")
                 for row in _repr_row_gen():
-                    _output.write(row)
-                return _output.getvalue()
+                    output.write(row)
+                return output.getvalue()
             else:
                 return json.dumps(self.class_details)
 
@@ -78,7 +76,7 @@ class Bootcampspot:
 
     @property
     def enrollment(self):
-        return self._enrollment
+        return self.__enrollment
 
     @enrollment.setter
     def enrollment(self, enrollmentId: int):
@@ -86,65 +84,62 @@ class Bootcampspot:
             msg = f'Invalid enrollmentId: {enrollmentId} not in your enrollments. Try one of these: {self.my_enrollments}'
             raise EnrollmentError(msg)
         elif not self._course == None:
-            _enroll_match = [
-                enrollment for enrollment in self.class_details if course['courseId'] == self._course][0]
-            if not enrollmentId == _enroll_match['enrollmentId']:
-                msg = f"Invalid courseId: {enrollmentId} did not match enrollmentId for set courseId. Did you mean {_enroll_match['enrollmentId']}"
+            enroll_match = [
+                enrollment for enrollment in self.class_details if course['courseId'] == self.__course][0]
+            if not enrollmentId == enroll_match['enrollmentId']:
+                msg = f"Invalid courseId: {enrollmentId} did not match enrollmentId for set courseId. Did you mean {enroll_match['enrollmentId']}"
                 raise EnrollmentError(msg)
         else:
-            self.enrollment = enrollmentId
-
-        '''
-        if enrollmentId
-
-        '''
+            self.__enrollment = enrollmentId
 
     @property
     def course(self):
-        return self._course
+        return self.__course
 
     @course.setter
     def course(self, courseId=int):
         if courseId not in self.my_courses:
             msg = f'Invalid courseId: {courseId} not in your courses. Try one of these: {self.my_courses}'
             raise CourseError(msg)
-        elif not self._enrollment == None:
-            _course_match = [
-                course for course in self.class_details if course['enrollmentId'] == self._enrollment][0]
-            if not courseId == _course_match['courseId']:
-                msg = f"Invalid courseId: {courseId} did not match courseId for set enrollmentId. Did you mean {_course_match['courseId']}"
+        elif not self.__enrollment == None:
+            course_match = [
+                course for course in self.class_details if course['enrollmentId'] == self.__enrollment][0]
+            if not courseId == course_match['courseId']:
+                msg = f"Invalid courseId: {courseId} did not match courseId for set enrollmentId. Did you mean {course_match['courseId']}"
                 raise CourseError(msg)
         else:
-            self._course = courseId
-            self._enrollment = [course['enrollmentId']
-                                for course in self.class_details if course['courseId'] == self._course][0]
+            self.__course = courseId
+            self.__enrollment = [course['enrollmentId']
+                                 for course in self.class_details if course['courseId'] == self.__course][0]
 
-    def _course_check(self, _course):
+    def __course_check(self, course):
         # Set courseId if not set
-        if not _course == None:
-            if _course not in self.my_courses:
-                msg = f'Invalid courseId: {_course} not in your courses. Try one of these: {self.my_courses}'
+        if not course == None:
+            if course not in self.my_courses:
+                msg = f'Invalid courseId: {course} not in your courses. Try one of these: {self.my_courses}'
                 raise CourseError(msg)
             else:
-                return _course
+                enroll_match = [course['enrollmentId']
+                                for course in self.class_details if course['courseId'] == self.__course][0]
+                return course, enroll_match
         else:
-            return self.course
+            return self.__course, self.__enrollment
 
-    def _enrollment_check(self, _enrollment):
+    def __enrollment_check(self, enrollment):
         # Set enrollmentId if not set
-        if not _enrollment == None:
-            if _enrollment not in self.my_enrollments:
-                msg = f"Invalid enrollmentId: {_enrollment} not in your enrollments. Try one of these: {self.my_enrollments}"
+        if not enrollment == None:
+            if enrollment not in self.my_enrollments:
+                msg = f"Invalid enrollmentId: {enrollment} not in your enrollments. Try one of these: {self.my_enrollments}"
                 raise EnrollmentError(msg)
             else:
-                return _enrollment
+                return enrollment
         else:
-            return self.enrollment
+            return self.__enrollment
 
-    def _call(self, endpoint=str, body=dict):
+    def __call(self, endpoint=str, body=dict):
         '''Grab response from endpoint'''
         response = requests.post(
-            f"{self._bcs_root}/{endpoint}", headers=self._head, json=body)
+            f"{self.__bcs_root}/{endpoint}", headers=self.__head, json=body)
         if response.status_code == 200:
             return response.json()
 
@@ -161,11 +156,11 @@ class Bootcampspot:
         },
         '''
 
-        courseId = self._course_check(courseId)
+        courseId = self.__course_check(courseId)
 
         body = {'courseId': courseId}
-        response = self._call('grades', body)
-        _grades = {}
+        response = self.__call('grades', body)
+        grades = {}
 
         def _value_check(grade):
             if type(grade) != str or grade == None:
@@ -174,38 +169,74 @@ class Bootcampspot:
                 return grade
 
         for assignment in response:
-            if 'Milestone' in assignment['assignmentTitle'] and not milestones:
+            if not milestones and 'Milestone' in assignment['assignmentTitle']:
                 pass
             else:
                 try:
-                    _grades[assignment['assignmentTitle']
-                            ][assignment['studentName']] = _value_check(assignment['grade'])
+                    grades[assignment['assignmentTitle']
+                           ][assignment['studentName']] = _value_check(assignment['grade'])
                 except:
-                    _grades[assignment['assignmentTitle']] = {
+                    grades[assignment['assignmentTitle']] = {
                         assignment['studentName']: _value_check(assignment['grade'])}
 
-        return _grades
+        return grades
 
-    def sessions(self, courseId=None, enrollmentId=None, career_ok=False):
-        '''Grabs All Sessions
+    def sessions(self, course_id=None, enrollment_id=None, career_ok=False, orientation_ok=False):
+        """Fetches session for course corresponding to courseId.
 
-        Response:
-            {currentWeekSessions: {'session': {
-                'name': 'Stuff', 'startTime': ISO8601,...}},
-            calendarSessions: {'session':{
-                'name':'Stuff','startTime':ISO8601,...}}
-                }
+        Calls the sessions endpoint to retrieve details about a session
 
-        '''
-        courseId = self._course_check(courseId)
-        enrollmentId = self._enrollment_check(enrollmentId)
+        Args:
+            courseId (int): takes an integer corresponding to a courseId
+            enrollmentId (int): takes an integer corresponding to an enrollmentId
+            career_ok (bool): takes in a boolean to limit results to academic sessions.
 
-        body = {'enrollmentId': enrollmentId}
+        Returns:
+            dict: The session details for the session matching session_id
+        """
+        if enrollment_id == None:
+            course_id, enrollment_id = self.__course_check(course_id)
+        else:
+            enrollment_id = self.__enrollment_check(enrollment_id)
+
+        body = {'enrollmentId': enrollment_id}
 
         # Perform API call
-        sessions = self._call('sessions', body=body)
+        sessions = self.__call('sessions', body=body)
 
         sessions_list = []
+
+        def session_append(session):
+            sessions_list.append({'id': session_info['id'],
+                                  'name': session_info['name'],
+                                  'short_description': session_info['shortDescription'],
+                                  'long_description': session_info['longDescription'],
+                                  'start_time': session_info['startTime'][:-1],
+                                  'end_time': session_info['endTime'][:-1],
+                                  'chapter': session_info['chapter'],
+                                  'context': session_type,
+                                  'classroom': session['classroom'],
+                                  'video_url_list': session['videoUrlList']
+                                  })
+
+        def mask_check(session_type):
+            if session_type == 'career':
+                if career_ok:
+                    return True
+                else:
+                    return False
+            elif session_type == 'orientation':
+                if orientation_ok:
+                    return True
+                else:
+                    return False
+            elif not course_id == None:
+                if session_info['courseId'] == course_id:
+                    return True
+                else:
+                    return False
+            else:
+                return True
 
         # Loop through current week sessions
         for session in sessions['calendarSessions']:
@@ -215,41 +246,26 @@ class Bootcampspot:
 
             # Filter on courseId and session code (to remove career entries)
             # Pull out session name, start time (ISO 8601 with UTC TZ removed)
-            if session_info['courseId'] == courseId:
-                if not career_ok and session_type == 'career':
-                    pass
-                else:
-                    sessions_list.append({'id': session_info['id'],
-                                          'name': session_info['name'],
-                                          'shortDescription': session_info['shortDescription'],
-                                          'longDescription': session_info['longDescription'],
-                                          'startTime': session_info['startTime'][:-1],
-                                          'chapter': session_info['chapter'],
-                                          'type': session_type
-                                          })
+            if mask_check(session_type):
+                session_append(session)
 
         return sessions_list
 
-    def attendance(self, courseId=None, by='session'):
-        '''Grab attendance
+    def attendance(self, course_id=None, by='session'):
+        """Fetches attendance for each student/course.
 
-            Returns dict of {studentName: ('absences', 'pending', 'remote')}
-        '''
-        '''
-        API Response Format:
-        {
-         'sessionName': str,
-         'studentName': str,
-         'pending': bool,
-         'present': bool,
-         'remote': bool,
-         'excused': bool | |None
-         }
-        '''
-        courseId = self._course_check(courseId)
+        Calls the attendance uri and encodes the attendance value as a category.
 
-        body = {'courseId': courseId}
-        response = self._call(endpoint='attendance', body=body)
+        Args:
+            course_id (int): takes an integer corresponding to a course_id
+
+        Returns:
+            dict: The student name, session name and attendance value.
+        """
+        course_id, enrollment_id = self.__course_check(course_id)
+
+        body = {'courseId': course_id}
+        response = self.__call(endpoint='attendance', body=body)
 
         def switch(session):
             if session['present'] == True and session['remote'] == False:
@@ -261,21 +277,21 @@ class Bootcampspot:
             elif session['present'] == False and session['excused'] == False:
                 return 'absent'
 
-        _attendance = {}
-        if by == 'session':
-            by_not = 'student'
-        else:
+        attendance = {}
+        by_not = 'student'
+
+        if by == 'student':
             by_not = 'session'
 
         for session in response:
             try:
-                _attendance[session[by+'Name']][session[by_not+'Name']] = switch(
+                attendance[session[by+'Name']][session[by_not+'Name']] = switch(
                     session)
             except:
-                _attendance[session[by+'Name']] = {
+                attendance[session[by+'Name']] = {
                     session[by_not+'Name']: switch(session)}
 
-        return _attendance
+        return attendance
 
     def session_details(self, session_id: int) -> Dict:
         """Fetches session details for session matching session_id.
@@ -283,18 +299,18 @@ class Bootcampspot:
         Calls the sessionDetail endpoint to retrieve details about a session
 
         Args:
-            session_id (int): takes an integer corresponding to a sessionId
+            session_id (int): takes an integer corresponding to a session_id
 
         Returns:
             dict: The session details for the session matching session_id
         """
 
         body = {'sessionId': session_id}
-        session_detail_response = self._call('sessionDetail', body)
+        session_detail_response = self.__call('sessionDetail', body)
 
         return session_detail_response['session']
 
-    def session_closest(self, enrollmentId=None, courseId=None) -> Dict:
+    def session_closest(self, course_id=None) -> Dict:
         """Fetches sessions list and returns nearest class to now.
 
         Based on the courseId and enrollmentId, it will call the sessions
@@ -303,28 +319,27 @@ class Bootcampspot:
         to that session['startTime']
 
         Args:
-            enrollmentId (int): takes an integer for the enrollmentId to pass
-            courseId (int): courseId for the session you'd like to retrieve.
+            enrollment_id (int): takes an integer for the enrollmentId to pass
+            course_id (int): courseId for the session you'd like to retrieve.
 
         Returns:
             dict: The course details for soonest class before or after current time.
 
         """
 
-        courseId = self._course_check(courseId)
-        enrollmentId = self._enrollment_check(enrollmentId)
+        course_id, enrollment_id = self.__course_check(course_id)
 
         now = datetime.utcnow()
         session_list = self.sessions(
-            enrollmentId=enrollmentId, courseId=courseId)
+            course_id=course_id)
 
-        def _closest_date(session_list: list, now):
+        def closest_date(session_list: list, now):
             return min(session_list, key=lambda x: abs(datetime.fromisoformat(x['startTime']) - now))
 
-        closest_session_id = _closest_date(session_list, now)['id']
+        closest_session_id = closest_date(session_list, now)['id']
 
         body = {'sessionId': closest_session_id}
-        session_detail_response = self._call('sessionDetail', body)
+        session_detail_response = self.__call('sessionDetail', body)
 
         return session_detail_response['session']['session']
 
